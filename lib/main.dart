230bbd3ccbf6e12ff44166bf9a87bf3368e985a9
mal <i.syafiq@thevoxasia.com>
2024-02-21 22:14:19 +0800

    EDIT : Firebase init // env // git append //

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:rent_it/firebase_options.dart';
+import 'package:rent_it/resources/strings/app_strings.dart';
 import 'package:rent_it/router/app_router.dart';
+import 'package:rent_it/services/top_level_services/main_services.dart/firebase_options_services.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseOptions? options = await getFirebaseOptions();
+  await Firebase.initializeApp(options: options);
   runApp(const ProviderScope(child: MyApp()));
 }
 
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Rent It',
+      title: AppStrings.appName,
       debugShowCheckedModeBanner: false,
       routerConfig: MyRouter().router,
       theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple), useMaterial3: true),
